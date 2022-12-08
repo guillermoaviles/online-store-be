54f4fa9bcb76fb89569d5fe5b063c4d1cd170396
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-08 14:39:48 -0600

    fixed Comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,14 @@
 const mongoose = require('../db/connection');
+
 const Schema = mongoose.Schema;
 
 
 // Comment Schema Model
 const commentSchema = new Schema(
     {
-        user: {
-            type: String,
-            default: "anonymous"
-        },
-        body: String
-    }
-);
+        title: String,
+        extract: String
+    });
 
 module.exports = mongoose.model('Comment', commentSchema);
 
