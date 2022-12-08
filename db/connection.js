c17b4382f1229beb6dd06a9eedf8904feed2c6ea
username <gjaviles7@gmail.com>
2022-12-08 12:19:41 -0500

    added connection setup

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+// Import Mongoose to interface with MongoDB
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.DATABASE_URL
+  process.env.NODE_ENV === `production`
+  ? process.env.DATABASE_URL
+  :`mongodb://localhost/online-store`;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, err => {
+    if(err) throw err;
+    console.log(`Connected to Mongo 🚀 at ${mongoURI}`)
+  })
+
+module.exports = mongoose;
