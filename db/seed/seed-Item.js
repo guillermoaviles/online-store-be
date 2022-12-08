7a6eb591279d61e99726d0c21c4d36a5191d3006
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-08 15:01:51 -0600

    seed-Comment is completed

---
+++ db/seed/seed-Item.js
@@ -0,0 +1,44 @@
+const Item = require('../models/Item.js')
+const Comment = require('../models/Comment.js')
+
+
+const itemRaw = require('../data/itemRaw.json')
+const commentRaw = require('../data/commentRaw.json');
+
+const itemData = itemRaw.map(item => {
+    const newItem ={}
+    newItem.title = item.title
+    newItem.description = item.description
+    newItem.images = item.images
+    newItem.price = item.price
+    return newItem
+})
+
+const commentData = commentRaw.map(comment => {
+    const newComment = {}
+    newComment.title = comment.title
+    newComment.extract = comment.extract
+    return newComments
+})
+
+Item.remove({})
+    .then(() => {
+        Item.create(itemData)
+            .then(items => {
+                console.log(itemData)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+
+Comment.remove({})
+    .then(() => {
+        Comment.create(commentData)
+            .then(comments => {
+                console.log(commentData)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
