844c440519afc65425ffe4403451d9826bfcc56c
username <gjaviles7@gmail.com>
2022-12-08 16:59:57 -0500

    comment controller

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,19 @@
 const mongoose = require('./connection');
 
 const Item = require('../models/Item');
+const Comment = require('../models/Comment');
 const itemsSeed = require('./data/itemRaw.json');
+const commentsSeed = require('./data/commentRaw.json');
 
 Item.deleteMany({}).then(() => {
     Item.create(itemsSeed).then(items => {
         console.log(items)
+    })
+})
+
+Comment.deleteMany({}).then(() => {
+    Comment.create(commentsSeed).then(comments => {
+        console.log(comments)
     })
 })
 
