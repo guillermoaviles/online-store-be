ae5b1d5846d9578904fc4f82cefaf96f90cc432a
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-08 15:18:28 -0600

    completed related-Comment

--- db/seed/seed-related-Comments.js
+++ db/seed/seed-related-Comments.js
@@ -1,2 +1,18 @@
 const Item = require('../../models/Item')
 const Comment = require ('../../models/Comment')
+
+const itemRaw = require('../data/itemRaw.json');
+const commentRaw = require('../data/commentRaw.json')
+
+Item.find({})
+    .then(items => {
+        console.log(items)
+        items.forEach(item => {
+            let itemJSON = itemRaw.find(itemJSON => itemJSON.title === itemJSON.title)
+            Comment.findOne({title: itemJSON.title})
+                .then(comment => {
+                    item.title = comment._id
+                    item.save()
+                })
+        })
+    })
